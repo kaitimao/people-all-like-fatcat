bf997d2bc6d456df3f078984a5b183a82c08ae47
kaitimao <mao712114@gmail.com>
2018-09-18 22:10:54 +0800

    22.4

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -1,41 +1,63 @@
-require 'net/http'
 require 'line/bot'
-
 class KamigoController < ApplicationController
- 	protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
+
   def webhook
-  	  # Line Bot API 物件初始化
-  	client = Line::Bot::Client.new { |config|
-    config.channel_secret = '43c2e6e6fd198d735ffe3e7bf3b29d29'
-    config.channel_token = 'PXXcr+o5uPWHsutFBgDu9Ax/daLc+bnAXylaTnQteEtTgkZY03K85yXMASVdY2pq5PwbIEqUCR1QIBgcqRPvYM4D+FFO4RoaIcBzhDgP0xBn81C0lKuXlX7WPaKhXMA6hQzUMaxfTE+xhDr1RbPCKwdB04t89/1O/w1cDnyilFU='
+    # 設定回覆文字
+    reply_text = keyword_reply(received_text)
 
-
-  	}
-  
-    # 取得 reply token
-    reply_token = params['events'][0]['replyToken']
-
-  	p "======這裡是 reply_token ======"
-    p reply_token 
-    p "============"
-
-    # 設定回覆訊息
-    message = {
-      type: 'text',
-      text: '好哦～好哦～'
-    }
-
-    # 傳送訊息
-    response = client.reply_message(reply_token, message)
-    p '========='
-	p response
-	p response.body
-	p '========='
-      
+    # 傳送訊息到 line
+    response = reply_to_line(reply_text)
+    
     # 回應 200
     head :ok
   end 
-  
+
+  # 取得對方說的話
+  def received_text
+    message = params['events'][0]['message']
+    message['text'] unless message.nil?
+  end
+
+  # 關鍵字回覆
+  def keyword_reply(received_text)
+    # 學習紀錄表
+    keyword_mapping = {
+      'QQ' => '神曲支援：https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s',
+      '我難過' => '神曲支援：https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s'
+    }
+    
+    # 查表
+    keyword_mapping[received_text]
+  end
+
+  # 傳送訊息到 line
+  def reply_to_line(reply_text)
+    return nil if reply_text.nil?
+    
+    # 取得 reply token
+    reply_token = params['events'][0]['replyToken']
+    
+    # 設定回覆訊息
+    message = {
+      type: 'text',
+      text: reply_text
+    } 
+
+    # 傳送訊息
+    line.reply_message(reply_token, message)
+  end
+
+  # Line Bot API 物件初始化
+  def line
+    @line ||= Line::Bot::Client.new { |config|
+      config.channel_secret = '43c2e6e6fd198d735ffe3e7bf3b29d29'
+      config.channel_token = 'PXXcr+o5uPWHsutFBgDu9Ax/daLc+bnAXylaTnQteEtTgkZY03K85yXMASVdY2pq5PwbIEqUCR1QIBgcqRPvYM4D+FFO4RoaIcBzhDgP0xBn81C0lKuXlX7WPaKhXMA6hQzUMaxfTE+xhDr1RbPCKwdB04t89/1O/w1cDnyilFU=
+'
+    }
+  end
+
+
   def eat
     render plain: "吃土啦"
   end 
